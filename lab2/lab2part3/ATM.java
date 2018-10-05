31be70e150045af308a053fb75c95a599c0204e0
Wojciech Rozowski <wkr1u18@soton.ac.uk>
2018-10-05 19:06:41 +0100

    lab2 complete

--- lab2/lab2part3/ATM.java
+++ lab2/lab2part3/ATM.java
@@ -1,11 +1,16 @@
 public class ATM {
-	Integer balance = new Integer(0);
+	Integer balance = new Integer(-1); //initialise balance with negative value, so it satisfies the condition of while loop
 	Toolbox myToolbox = new Toolbox();
 	
 	public void withdraw() {
 		System.out.println("How much would you like to withdraw?");
-		Integer toWithdraw = new Integer(0);
-		toWithdraw = myToolbox.readIntegerFromCmd();
+		Integer toWithdraw = new Integer(-1); //same case as with balance
+		while (toWithdraw<0) {
+			toWithdraw = myToolbox.readIntegerFromCmd();
+			if(toWithdraw>balance) {
+				toWithdraw = -1; //when toWithdraw is bigger than balance, then set it's value to negative
+			}
+		}
 		balance -= toWithdraw; //substract ammunt to withdraw from balance
 		System.out.print("Your new balance is: ");
 		System.out.println(balance);
@@ -13,8 +18,10 @@ public class ATM {
 		
 	public void deposit() {
 		System.out.println("How much would you like to deposit?");
-		Integer toDeposit = new Integer(0);
-		toDeposit = myToolbox.readIntegerFromCmd();
+		Integer toDeposit = new Integer(-1);
+		while(toDeposit<0) {
+			toDeposit = myToolbox.readIntegerFromCmd();
+		}
 		balance += toDeposit;
 		System.out.print("Your new balance is: ");
 		System.out.println(balance);
@@ -32,7 +39,9 @@ public class ATM {
 	public void go() {
 		System.out.println("Welcome to online ATM banking");
 		System.out.println("How much do you want in your account?");
-		balance = myToolbox.readIntegerFromCmd();
+		while(balance<0) {
+			balance = myToolbox.readIntegerFromCmd();
+		}
 		System.out.println(balance);
 		while(true) {
 			String welcome = "What do you want to do?\n" +
