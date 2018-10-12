db64269ec4d76fca5793a2cf3c31ae1400089633
Wojciech Rozowski <wkr1u18@soton.ac.uk>
2018-10-12 12:25:26 +0100

    Fixing typos

--- lab3/lab3part1/SmartCard.java
+++ lab3/lab3part1/SmartCard.java
@@ -7,23 +7,23 @@ public class SmartCard{
     private String ownerName;
     private Boolean isStaff;
     
-    //constructor method setting ownerName to a given string
+    //Constructor method setting ownerName to a given string
     public SmartCard(String initialName) {
         ownerName = initialName;
         isStaff = false;
     }
     
-    //accessor method for ownerName field
+    //Accessor method for ownerName field
     public String getOwner() {
         return ownerName;
     }
     
-    //acessor method for isStaff field
+    //Acessor method for isStaff field
     public Boolean isStaff() {
         return isStaff;
     }
     
-    //mutator method for isStaff field
+    //Mutator method for isStaff field
     public void setStaff(Boolean newState) {
         isStaff = newState;
     }
