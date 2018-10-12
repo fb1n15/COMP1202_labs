c70b75850d5bc0225b5608699fabd3cc117917ac
Wojciech Rozowski <wkr1u18@soton.ac.uk>
2018-10-12 14:55:04 +0100

    small update to pass test harness

--- lab2/lab2part1/ATM.java
+++ lab2/lab2part1/ATM.java
@@ -12,7 +12,7 @@ public class ATM {
 	public void go() {
 		System.out.println("Welcome to online ATM banking");
 		System.out.println("How much do you want in your account?");
-		//System.out.println("Enter your number");
+		System.out.println("Enter your number");
 		//"Enter your number" shown by readIntegerFromCmd() is not acknowledged in lab2part2 test, so it needs to be shown again.
 		balance = myToolbox.readIntegerFromCmd();
 		System.out.println(balance);
